58c46e922127a76358ebf1e80a5d548bf77a0c96
Yu Hua Cheng <yuhuac@twitter.com>
2021-03-13 21:58:19 +0800

    Fix the incorrect slider's position

--- src/CorrectElement.js
+++ src/CorrectElement.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { useSpring, animated } from "react-spring";
-import "./CorrectElement.css";
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+import './CorrectElement.css';
 
 function CorrectElement() {
   const props = useSpring({
-    from: { marginTop: -750 },
-    to: { marginTop: -680 },
+    from: { marginTop: '-20vh' },
+    to: { marginTop: '1vh' },
   });
 
   return (
-    <div>
+    <div style={{ background: '#a4dd00' }}>
       <animated.div style={props}>
         <h1 className="CorrectBar">
           Brawo! <br />
