f548b90b0cb6ee2f2da8d3cab65f6589fe181bde
Kasia Gierat <kasia.gierat@gmail.com>
2021-03-15 19:44:58 +0800

    homePage completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import RollDice from "./RollDice";
 import HomePage from "./HomePage";
 
 const App = () => {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     numberOfDices: 3,
-  //   };
-  // }
-
-  // const home = () => (
-  //   <h1>
-  //     Cześć Pola! <br />
-  //     Grasz w dodawanie?
-  //   </h1>
-  // );
-
   const roll2dices = () => {
     return <RollDice numOfDices={2} />;
   };
@@ -28,15 +14,10 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-        {/* <div>
-          <Link to="/2kostki">Dodawanie od 1-12</Link>
-          <Link to="/3kostki">Dodawanie od 1-18</Link>
-        </div> */}
         <main>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/2kostki" component={roll2dices} />
           <Route exact path="/3kostki" component={roll3dices} />
-          {/* <RollDice numOfDices={this.state.numberOfDices} /> */}
         </main>
       </div>
     </Router>
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,19 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./HomePage.css";
+// import img from "../public/robin.png";
 
 const HomePage = (props) => {
   return (
-    <div>
-      <h1>
-        Cześć Pola! <br />
-        Grasz w dodawanie?
-      </h1>
+    <div className="HomePage">
       <div>
-        <ul>
-          <Link to="/2kostki">Dodawanie od 1-12</Link>
-          <Link to="/3kostki">Dodawanie od 1-18</Link>
-        </ul>
+        <h1>
+          Cześć Pola! <br />
+          Grasz w dodawanie?
+        </h1>
       </div>
+
+      {/* <div> */}
+      <div>
+        <Link to="/2kostki" className="Link">
+          <div className="Back-for-img1">
+            <img src={require("./bird.png").default} className="Image"></img>
+          </div>
+          <p>Liczby od 1-12</p>
+        </Link>
+      </div>
+
+      <div>
+        <Link to="/3kostki" className="Link">
+          <div className="Back-for-img2">
+            <img
+              src={require("./squirrel.png").default}
+              className="Image"
+              style={{ color: "red" }}
+            ></img>
+          </div>
+          <p>Liczby od 1-18</p>
+        </Link>
+      </div>
+      {/* </div> */}
     </div>
   );
 };
